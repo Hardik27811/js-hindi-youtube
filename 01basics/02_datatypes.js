771282d0702c407152ab3934b2724112b2ef7860
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-03 09:33:46 +0000

    datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as a newer version
+
+// alert( 3 + 3) we r using node js not  browser
+
+let name="hardik"
+let age=23
+let isLoggedIn=false
+
+//number=> 2 to pow 53
+//bigInt
+//String =>""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique 
+
+//object
+
+console.log(typeof"hardik") //String
+console.log(typeof 23)   //number
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
