aa1ae8644a7f951bcac5ebfa713a5fafcb59e24e
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-03 13:15:09 +0000

    summ

---
+++ 01basics/datatype_summary.js
@@ -0,0 +1,51 @@
+//***************************************  JAVASCRIPT Dynamically typed lang ********************************* */
+//Dynamically-typed languages are those (like JavaScript) where the interpreter assigns variables a type at runtime 
+// based on the variable's value at the time.
+
+
+
+// how data is stored in memory and the way to access those data
+// on the basis of these it is categorised into two types
+/*
+1) Primitive -7 (call by value, ..data copied)
+number
+bigInt
+string
+boolean
+null  -- empty
+undefined --variable declared. .. memory space declared but value not
+symbol -- to make a value UNIQUE
+
+
+
+2) Non Primitive or Refrence type
+Array
+Objects
+functions
+
+*/
+
+// const id=Symbol('123')
+// const anotherid=Symbol('123')
+// console.log(id === anotherid);
+
+const heros=["apple","banana","cherry"]; //array
+
+let myObj={
+    name: "Raj",
+    age: 22
+}//object
+
+
+
+
+const myFunc=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros); //object
+console.log(typeof myObj);//object
+console.log(typeof myFunc); //function object
+
+
+
