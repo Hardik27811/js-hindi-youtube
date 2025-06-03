7c3461be731ff9bbefb4de596c71f154b6e7af4f
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-03 12:28:09 +0000

    comaprison

---
+++ 01basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log("2" >1); //true
+// console.log("02">1 ); // true // cause js converts string into number for comparison
+/*
+console.log(null > 0); //false
+console.log( null == 0); //false
+console.log(null >=0); // true
+
+console.log(undefined >0); //false
+console.log(undefined <0); //false
+console.log(undefined ==0); //false in every case of undefined
+*/
+
+
+
+//reson == and comaprison works different , comaprison convert null to a num, treating it as 0
+//thats why >=0 is true and >0 is false
+
+
+//SO bascically == and >,<,>=,<= is diff in js
+
+
+
+// strict check ******************* ( ===  ) value as well as datatype ******************************
+console.log("2" === 2); //false
+
+
+
+
