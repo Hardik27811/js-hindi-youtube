263935b469cb9b0955007dd3553c22805b50db5f
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-04 09:58:44 +0000

    math

---
+++ 01basics/06_numsandmath.js
@@ -0,0 +1,50 @@
+const score=100.3456
+// console.log(score);   //100
+
+const balance=new Number(100.989)
+// console.log(balance);  //[Number: 100]
+// console.log(balance.valueOf()); //100
+
+// console.log(balance.toString().length);  //3
+//console.log(balance.toFixed(2));   //100.99
+
+const b=23.45566677
+//console.log(b.toPrecision(4)); //23.46
+
+const c=100000
+//console.log(c.toLocaleString('en-IN'));  1,00,000
+
+//console.log(Number.MAX_VALUE); ///1.7976931348623157e+308
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++. MATH ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);  //Object [Math] {}
+// console.log(Math.abs(-3));  //3
+// console.log(Math.round(4.8));  //5
+// console.log(Math.ceil(3.0));  //3
+// console.log(Math.ceil(3.01));  //4
+// console.log(Math.floor(5.9)); //5
+// console.log(Math.sqrt(9)); //3
+
+
+
+console.log(Math.max(3,4,5,32,56));
+console.log(Math.min(3,4,5,32,56));
+
+console.log(Math.random());
+
+const min=1
+const max=6
+console.log(Math.floor(Math.random()*(max - min + 1)+min)) // 1 and 6 included
+
+
+
+
+
+
+
+
+
+
+
