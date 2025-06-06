177270f269a59195897306bec1be15fc4e3016af
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-06 10:27:37 +0000

    add

---
+++ 01basics/07_dates.js
@@ -0,0 +1,28 @@
+const mydate=new Date()
+// console.log(mydate);  //2025-06-04T10:10:27.621Z
+
+// console.log(mydate.toString()); //Wed Jun 04 2025 10:10:27 GMT+0000 (Coordinated Universal Time)
+
+// console.log(mydate.toDateString()) //Wed Jun 04 2025
+
+// console.log(mydate.toLocaleString());  //. 6/4/2025, 10:13:39 AM 
+
+// console.log(typeof mydate); //object
+
+const date=new Date(2023,0,5)
+console.log(date.toDateString()); //Thu Jan 05 2023
+
+const timestamp=Date.now()
+
+console.log(timestamp); //1749032508146
+
+
+
+
+
+
+
+
+
+
+
