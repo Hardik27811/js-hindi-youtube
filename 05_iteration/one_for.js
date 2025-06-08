31084895faf7c5c9357c138c5551d7c457c92f2c
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-08 04:54:38 +0000

    new

---
+++ 05_iteration/one_for.js
@@ -0,0 +1,30 @@
+// for
+
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+    
+// }
+
+
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log(i +"*" + j + "="+ i*j);
+        
+//     }
+// }
+
+
+//+++++++++++++++++++ break and continue    ++++++++++++++++++++++++++++++
+
+for(let i=0; i<=10;i++){
+    if( i===5){
+        continue;
+    }
+    else if(i==8){
+        break;
+    }
+    else{
+    console.log(i);
+    }
+    
+}
