f80e4b0c1a43ad1dbf2409c04b66476960e007bb
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-08 04:55:42 +0000

    new

---
+++ 05_iteration/two_while.js
@@ -0,0 +1,25 @@
+// while
+
+// i=0
+// while (i<=10) {
+//     console.log(i);
+//     i++
+    
+// }
+
+
+// array
+
+const myarr=[1,4,4,5,6,6,7]
+i=0
+// while(i<myarr.length){
+//     console.log(myarr[i]);
+//     i++
+// }
+
+let score=11
+do{
+    console.log(score); //11
+    score++
+    
+}while(score<=10)
