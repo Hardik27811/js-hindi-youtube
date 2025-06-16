ae12608431e262a512a2128bfadb43e5f0d8b2cd
Hardik27811 <kumarankit2002dseu@gmail.com>
2025-06-16 05:16:20 +0000

    add

---
+++ 06_DOM/01_ex.js
@@ -0,0 +1,23 @@
+/*
+
+title.style.backgroundColor="red"
+'red'
+
+
+
+title.textContent
+'DOM learning on chai aur code hidden text'
+
+title.innerText
+'DOM learning on chai aur code'
+
+title.innerHTML
+'DOM learning on chai aur code <span style="display: none;">hidden text</span>'
+
+
+document.querySelector('h2')
+
+document.getElementsByClassName('list-item')
+
+
+*/
